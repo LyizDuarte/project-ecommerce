9986ba1b0cd45eda9b2c10b81cdf9e8894d7091e
Luiz Duarte <looduarte21@gmail.com>
2024-11-18 19:39:26 -0300

    feat: metodo gravar com validacao de estoque na controller

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -1,33 +1,60 @@
 const PedidoModel = require("../models/pedidoModel")
 const PedidoItemModel = require("../models/pedidoItemModel")
-
 class PedidoController {
   async gravar(req, res) {
     let ok = false
     let msg = ""
+    let listaValidacao = []
     if (req.body.length > 0) {
-      //gerar pedido
-      let pedido = new PedidoModel()
-      let pedidoId = await pedido.gravar()
-      //iniciar a geracao dos itens
-      for (let i = 0; i < req.body.length; i++) {
-        let item = new PedidoItemModel()
-        //vinculo do pedido
-        item.pedidoId = pedidoId
-        item.produtoId = req.body[i].produtoId
-        item.pedidoItemQuantidade = req.body[i].quantidade
-        item.pedidoItemValor = req.body[i].produtoValor
-        item.pedidoItemValorTotal =
-          req.body[i].produtoValor * req.body[i].quantidade
+      let listaProdutos = []
+      //validação de estoque
 
-        await item.gravar()
+      for (let i = 0; i < req.body.length; i++) {
+        let produtoId = req.body[i].produtoId
+        let quantidade = req.body[i].quantidade
+        let produto = new ProdutoModel()
+        if ((await produto.validarEstoque(produtoId, quantidade)) == false) {
+          listaValidacao.push(produtoId)
+        }
       }
-      ok = true
-      msg = "Pedido gravado com sucesso"
+
+      if (listaValidacao.length == 0) {
+        //gerar pedido
+        let pedido = new PedidoModel()
+        let pedidoId = await pedido.gravar()
+        //inicar a geração dos itens
+        for (let i = 0; i < req.body.length; i++) {
+          let item = new PedidoItemModel()
+          //vinculo do pedido;
+          item.pedidoId = pedidoId
+          item.produtoId = req.body[i].produtoId
+          item.pedidoItemQuantidade = req.body[i].quantidade
+          item.pedidoItemValor = req.body[i].produtoValor
+          item.pedidoItemValorTotal =
+            req.body[i].produtoValor * req.body[i].quantidade
+
+          await item.gravar()
+
+          //atualizar estoque
+          let produto = new ProdutoModel()
+          produto = await produto.buscarProduto(req.body[i].produtoId)
+
+          produto.produtoQuantidade =
+            produto.produtoQuantidade - req.body[i].quantidade
+
+          await produto.gravar()
+        }
+
+        ok = true
+        msg = "Pedido gravado com sucesso!"
+      } else {
+        msg = "Erro durante a validação de estoque"
+      }
     } else {
-      msg = "Nenhum produto para gravar"
+      msg = "Nenhum produto para gravar!"
     }
-    res.send({ ok: ok, msg: msg })
+
+    res.send({ ok: ok, msg: msg, listaValidacao: listaValidacao })
   }
 
   async listar(req, res) {
